9cf8313b3513bdf86fd71dbb3fff8f1a89280003
Kristian Sons <kristian.sons@supporting.com>
2010-12-27 12:03:21 +0000

    Added Web3D test suite to data directory

---
+++ data/web3d/convert2X3D.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+myfp=`which $0`
+mydir=`dirname $myfp`
+
+echo $mydir
+
+for f in $mydir/*.x3db
+do
+	echo "Processing - $f"
+	if x3db2x3d "$f" > /dev/null; then
+		echo "succesfull"
+	fi
+done
