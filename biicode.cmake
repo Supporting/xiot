f1fc1595da6576f707a9d30cbfcdb9c426217b11
Kristian Sons <kristian.sons@dfki.de>
2015-05-10 12:38:18 +0200

    Build xiot static by default

--- biicode.cmake
+++ biicode.cmake
@@ -1,5 +1,3 @@
-SET(BII_LIB_TYPE SHARED)
-MESSAGE(STATUS "Executables: " ${BII_BLOCK_EXES})
 LIST(REMOVE_ITEM BII_LIB_SRC tests/Argument_helper.cpp)
 LIST(APPEND BII_tests_createEventLog_SRC tests/X3DLogNodeHandler.cpp)
 
